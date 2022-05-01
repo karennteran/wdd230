eae5e55c8ce5b8beeaa84a9ad451b9d7933d754e
karennteran <terankarenn@gmail.com>
2022-05-01 01:52:29 -0400

    new update

---
+++ lesson2/js/lastmodified.js
@@ -0,0 +1,6 @@
+document.getElementById("lastMod").innerHTML = document.lastModified;
+
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
